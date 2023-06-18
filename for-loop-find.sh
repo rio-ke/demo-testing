708a68223e83ead154230e3eeec23227930b494b
rio-ke <88568938+rio-ke@users.noreply.github.com>
2023-06-18 18:54:06 +0530

    Update for-loop-find.sh

--- for-loop-find.sh
+++ for-loop-find.sh
@@ -1,4 +1,4 @@
-for d in /home/Documents/test/*/ ; do 
+for d in /home/$user/Documents/ken/*/ ; do 
     fname=${d##*/}  # Strip /home/Documents/test/
     fname=${fname%/}    # Strip the trailing /
     # Note that d is already the full directory name you want to use
